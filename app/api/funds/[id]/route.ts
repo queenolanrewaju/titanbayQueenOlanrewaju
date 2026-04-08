1b506d5ab1c9ec9cabe193fda22f3d2795665876
queolanr <queen.olanrewaju@publicissapient.com>
2026-04-08 19:20:53 +0100

    adding post, put, and get requests for funds, tested and working with postman

--- app/api/funds/[id]/route.ts
+++ app/api/funds/[id]/route.ts
@@ -15,3 +15,19 @@ export async function GET(request: Request, { params }: { params: Promise<{ id: 
         return NextResponse.json({ error: 'Server error' }, { status: 500 }); //if there is issue with the server it will return a status 500
     }
 }
+//update a fund
+export async function PUT(request:Request, { params }: { params: Promise<{ id: string }> }) {
+    try{
+        const {id} = await params;
+        const { name, vintage_year, target_size_usd, status } = await request.json();
+        const result = await pool.query(
+        'UPDATE funds SET name = $1, vintage_year = $2,  target_size_usd = $3, status = $4 WHERE id = $5 RETURNING *', [name, vintage_year, target_size_usd, status, id]
+        );
+
+    return NextResponse.json(result.rows[0], { status: 200 }); // 200 for UPDATE
+
+    }catch (err){
+        return NextResponse.json({  error: err instanceof Error ? err.message : 'Server error' }, {status:500});
+    }
+
+}
--- app/api/funds/route.ts
+++ app/api/funds/route.ts
@@ -1,5 +1,28 @@
 import { NextResponse } from 'next/server';
+import pool from '@/db';
 
 export async function GET(request: Request) {
+    try {
+        const result = await pool.query('SELECT * FROM funds');
+        return NextResponse.json(result.rows, { status: 200 });//returns all the funds
+    } catch (err) {
+        return NextResponse.json({ error: 'Server error' }, { status: 500 }); //if there is issue with the server it will return a status 500
+    }
+}
+
+export async function POST(request:Request){
+    try{
+        const { name, vintage_year, target_size_usd, status } = await request.json();
+        const result = await pool.query(
+        'INSERT INTO funds (name, vintage_year, target_size_usd, status) VALUES ($1, $2, $3, $4) RETURNING *', [name, vintage_year, target_size_usd, status]
+        );
+
+    return NextResponse.json(result.rows[0], { status: 201 }); // 201 for POST
+
+    }catch (err){
+        return NextResponse.json({ error: 'server error'}, {status:500});
+    }
 
 }
+
+
